0e95e57964d9e17a8382ee2ea712448d45fa9652
Oriol <oriol@macbook.(none)>
2014-09-25 20:48:12 +0200

    Notifications + groups

--- client/js/sovietApp.js
+++ client/js/sovietApp.js
@@ -9,20 +9,36 @@ var sovietApp = angular.module('sovietApp', [ 'ngRoute', 'ui.bootstrap' ])
 				})
 				.otherwise({ redirectTo: '/' });
 		}])
+	.filter('count', function () {
+			return function (input) {
+				var out = input.length;
+				if (!out)
+					out = 0;
+				return out;
+			};
+		})
+	.filter('empty', function () {
+			return function (s, alt) {
+				return s || alt;
+			};
+		})
 	.controller('mainController', [ '$scope', '$route',
 		function ($scope, $route) {
-			$scope.customers = [
-				{ name: 'Josep', city: 'Barcelona' },
-				{ name: 'Maria', city: 'Girona' }
-			];
+			$scope.groups = {
+				1: { name: 'Col·lectiu Diagonal' },
+				3: { name: 'Comitè Universitat' },
+				4: { name: 'Comitè Central' }
+				};
+			$scope.user = { name: 'Oriol', groups: [1, 3, 4], role: 'admin' };
 			$scope.isCollapsed = false;
 		}])
 	.controller('headerController', [ '$scope', '$route',
 		function ($scope, $route) {
 			$scope.notifications = [
-				{ description: 'Notificació 1', action: 'index.html#/' },
-				{ description: 'Notificació 2', action: 'index.html#/profile' },
-				{ description: 'Notificació 3', action: 'index.html#/group' }
+				{ description: 'Notificació 1', action: 'index.html#/', seen: true },
+				{ description: 'Notificació 2', action: 'index.html#/profile', seen: false },
+				{ description: 'Notificació 3', action: 'index.html#/group', seen: true }
 			];
 		}]);
+
 
